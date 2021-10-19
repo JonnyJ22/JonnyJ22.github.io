c4fae52880c31fc86362bbf6e64fa3bbc4eb9a3f
sleepydog <89933693+sleepydogdog@users.noreply.github.com>
2021-10-18 23:02:44 -0400

    Update golfScorecard.js

--- assignments/golfScorecard.js
+++ assignments/golfScorecard.js
@@ -16,6 +16,18 @@ for(let i=1; i<=18; i++) {
   elem[i].children[4].children[0].onclick = function(){add1(elem[i]);};
 }
 
+for(let i=1; i<=18; i++) {
+  // console.log(i);
+  elem[i] = document.getElementById(i.toString());
+  elem[i].children[4].children[1].onclick = function(){subtract1(elem[i]);};
+}
+
+for(let i=1; i<=18; i++) {
+  // console.log(i);
+  elem[i] = document.getElementById(i.toString());
+  elem[i].children[4].children[2].onclick = function(){clear1(elem[i]);};
+}
+
 // create an "add1" function
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") 
@@ -24,6 +36,26 @@ function add1 (elem) {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+
+function subtract1 (elem) {
+  if(elem.children[2].innerHTML == "0") 
+    elem.children[2].innerHTML = "0";
+  else {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+function clear1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "0";
+  else {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore = 0;
   }
 }
 
